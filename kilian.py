d2ef3a372fa98a3668f69c451576d74896392de3
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2022-12-27 23:56:03 +0100

    argument parsing

--- kilian.py
+++ kilian.py
@@ -1,2 +1,30 @@
+"""
+    File name: kilian.py
+    Author: Adrian Vinojcic
+"""
+
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--token",
+        type=str,
+        required=False,
+        dest='token',
+        help="Provide the Discord Bot Token as token."
+    )
+    parser.add_argument(
+        "-i",
+        action="store_true",
+        help="Initialize the slash commands(only necessary on first startup."
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    print("Hi")
+    args = parse_args()
+    print(args.token)
+    print(args.i)
+# TODO: implement a file where the config can be defaulted into
