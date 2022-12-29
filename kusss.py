91e4b74565187a1955f45676b945089ffa40d7ce
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2022-12-29 19:08:10 +0100

    Add exception handling to KUSSS calendar download

--- kusss.py
+++ kusss.py
@@ -13,11 +13,27 @@ TOKEN = "token"
 lang = "de"
 
 
-def calendar(url: str):
-    url = urlparse(url)
-    if url.netloc == kusss and url.path == calendar_path:
+def calendar(link: str):
+    url = urlparse(link)
+    if url.netloc != kusss or url.path != calendar_path:
+        raise InvalidURLException(link, "Invalid URL.")
+
+    try:
         token = parse_qs(url.query)[TOKEN][0]
-        url = "https://{}{}?token={}&lang={}".format(kusss, calendar_path, token, lang)
-        return token, requests.get(url).content
-    else:
-        raise
+    except Exception:
+        raise InvalidURLException(link, "Token not in query.")
+
+    link = "https://{}{}?{}={}&lang={}".format(kusss, calendar_path, TOKEN, token, lang)
+    content = requests.get(link).content
+    if len(content) == 0:
+        raise InvalidURLException(link, "Token malformed.")
+    return token, content
+
+
+class InvalidURLException(Exception):
+    """Exception raised for invalid calendar links provided by the user."""
+
+    def __init__(self, link, message="Provided link is not valid"):
+        self.message = message
+        self.link = link
+        super().__init__(self.message)
