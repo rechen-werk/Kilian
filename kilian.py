f4fada0352458a73ec4193f362ea59603a883ffe
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2022-12-31 17:59:24 +0100

    start to work on a few commands, happy new year

--- kilian.py
+++ kilian.py
@@ -30,17 +30,70 @@ if __name__ == '__main__':
     args = parse_args()
     bot_token = args.token if args.token is not None else read_token_file()
 
-    bot = interactions.Client(token=bot_token)
+    bot = interactions.Client(token=bot_token, intents=interactions.Intents.ALL)
 
     @bot.command()
     @interactions.option(description="Provide the calendar link from KUSSS here.")
     async def kusss(ctx: interactions.CommandContext, link: str):
         """Take advantage of the features provided by Kilian™."""
-        user_token = uni.token(link)
-        courses = uni.courses(user_token)
-        s = ""
-        for course in courses:
-            s += course.name + "\n"
-        await ctx.send(s)
+        try:
+            user_token = uni.token(link)
+            user_id = ctx.author.id
+
+            # TODO: save *user_id* and *user_token* into database
+            # TODO: map anonymous roles to user
+
+            await ctx.send("Welcome on board " + ctx.author.name + "!")
+        except uni.InvalidURLException as ex:
+            await ctx.send(ex.message, ephemeral=True)
+
+    @bot.command()
+    async def unkusss(ctx: interactions.CommandContext):
+        """Unsubscribe from the awesome features provided by Kilian™."""
+        user_id = ctx.author.id
+
+        # TODO: delete all rows which contain *user_id* from the database
+        # TODO: delete anonymous role mappings from user
+
+        await ctx.send("A pity to see you leave " + ctx.author.name + ". You can join the club anytime with `/kusss`!")
+
+    @bot.command()
+    @interactions.option(description="Role you want to ping.")
+    @interactions.option(description="Message content goes here.")
+    async def ping(ctx: interactions.CommandContext, role: interactions.Role, content: str = ""):
+        """Ping everyone partaking that subject."""
+        role_id = role.id
+
+        # TODO: check if said role is a managed role and send an empheral errormessage otherwise
+        # TODO: get all users mapped to said *role_id*
+        users_with_anonymous_role = set()
+
+        # TODO: ping all users with said role in one message
+        ping_string = ""
+        for user in users_with_anonymous_role:
+            yield
+
+        message = await ctx.send(ping_string)
+        await message.edit(role.mention + "\n" + content)
+
+        # POSSIBLE ERROR: too many users, so that not all pings fit in one message
+        # POSSIBLE SOLUTION TO ERROR: give all pinged users the role temporarily, ping them, and remove the role
+
+
+    @bot.event()
+    async def on_message_create(message: interactions.Message):
+        if message.author.id == bot.me.id:
+            return
+
+        mentioned_roles = message.mention_roles
+        # TODO: check if a role is a managed role
+        # TODO: get all users mapped to said *role_id*
+        users_with_anonymous_role = set()
+
+        # TODO: ping all users with said role in one message
+        await message.reply("ok ok")
+
+    # await message.get_channel().send(message.author.mention)
+
 
     bot.start()
