9a0f41c946df11fe71e1232205b9d576c53e5e1d
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2023-01-02 02:01:42 +0100

    kusss.py is way more structured now, 3 clear methods I hope

--- kusss.py
+++ kusss.py
@@ -4,85 +4,10 @@
     This part is responsible for fetching users calendars.
 """
 
-from io import StringIO
-# from datetime import datetime
 from urllib.parse import urlparse, parse_qs
 import requests
 from icalendar import Calendar
-from lxml import html
-import pandas
-
-__kusss__ = "www.kusss.jku.at"
-__calendar_path__ = "/kusss/published-calendar.action"
-__TOKEN__ = "token"
-__lang__ = "de"
-
-
-def all_courses():
-    content = requests.get("https://kusss.jku.at/kusss/coursecatalogue-search-lvas.action?").content
-    table = html.fromstring(content).xpath("/html/body/table/tr[2]/td[2]/table/tr[2]/td/div/table[2]")[0]
-    table_string = html.tostring(table).decode()
-    df = pandas.read_html(StringIO(table_string), decimal=",", thousands=".")[0]
-    # df.to_csv("2022.csv", decimal=",", sep=";", index=False)
-    kusss_courses = set()
-    for _, row in df.iterrows():
-        kusss_courses.add(Course(lva_type=row["Typ"], lva_name=row["LVA-Titel"], teacher=row["LeiterIn"], lva_nr=row["LVA-Nr."], semester=row["Sem."]))
-    return kusss_courses
-
-
-def courses(user_token: str):
-    user_courses = set()
-    i_calendar = __calendar__(user_token)
-    cal = Calendar.from_ical(i_calendar)
-    for component in cal.walk():
-        if component.name == "VEVENT":
-            summary = component.get('summary').split(" / ")
-
-            # delete tag of exams
-            if len(summary) > 3:
-                summary = summary[1:]
-
-            user_courses.add(
-                Course(
-                    lva_type=summary[0][:2],
-                    lva_name=summary[0][3:],
-                    teacher=summary[1],
-                    lva_nr=summary[2][1:7],
-                    semester=summary[2][8:13]
-                )
-            )
-
-            # parse data for timetable
-            # start: datetime = component.get('dtstart').dt
-            # end: datetime = component.get('dtend').dt
-            # location: str = component.get('location')
-    return user_courses
-
-
-def __calendar__(user_token: str):
-    _, content = __token_and_content__(f"https://{__kusss__}{__calendar_path__}?{__TOKEN__}={user_token}&lang={__lang__}")
-    return content
-
-
-def token(link: str):
-    user_token, _ = __token_and_content__(link)
-    return user_token
-
-
-def __token_and_content__(link: str):
-    url = urlparse(link)
-    if url.netloc != __kusss__ or url.path != __calendar_path__:
-        raise InvalidURLException(link, "Invalid URL.")
-
-    try:
-        user_token = parse_qs(url.query)[__TOKEN__][0]
-    except Exception:
-        raise InvalidURLException(link, "Token not in query.")
-    link = f"https://{__kusss__}{__calendar_path__}?{__TOKEN__}={user_token}&lang={__lang__}"
-    content = requests.get(link).content
-    if len(content) == 0:
-        raise InvalidURLException(link, "Token malformed or deprecated.")
-    return user_token, content
+from bs4 import BeautifulSoup
 
 
 class InvalidURLException(Exception):
@@ -94,16 +19,13 @@ class InvalidURLException(Exception):
         super().__init__(self.message)
 
 
-class Course:
-    def __init__(self, lva_type: str, lva_name: str, teacher: str, lva_nr: str, semester: str):
-        self.type = lva_type
-        self.name = lva_name
-        self.teacher = teacher
+class CourseKey:
+    def __init__(self, lva_nr: str, semester: str):
         self.lva_nr = lva_nr
         self.semester = semester
 
     def __eq__(self, other):
-        return isinstance(other, Course) and self.lva_nr == other.lva_nr and self.semester == other.semester
+        return isinstance(other, CourseKey) and self.lva_nr == other.lva_nr and self.semester == other.semester
 
     def __ne__(self, other):
         return not __eq__(other)
@@ -111,10 +33,97 @@ class Course:
     def __hash__(self):
         return hash(self.lva_nr + self.semester)
 
-# Class data structure for timetables later eventually
-# class Class:
-#    def __init__(self, start: datetime, end: datetime, course: Course, location: str):
-#        self.start = start
-#        self.end = end
-#        self.course = course
-#        self.location = location
+
+class Course(CourseKey):
+    def __init__(self, lva_nr: str, semester: str, lva_type: str, lva_name: str, teachers: list[str], link: str):
+        super().__init__(lva_nr, semester)
+        self.lva_type = lva_type
+        self.lva_name = lva_name
+        self.teachers = teachers
+        self.link = link
+
+
+class Student:
+    def __init__(self, discord_id: str, calendar_link: str, courses: list[CourseKey], matr_nr: str = None):
+        self.discord_id = discord_id
+        self.calendar_link = calendar_link
+        self.courses = courses
+        self.matr_nr = matr_nr
+
+    def __eq__(self, other):
+        return isinstance(other, Student) and self.discord_id == other.discord_id
+
+    def __ne__(self, other):
+        return not __eq__(other)
+
+    def __hash__(self):
+        return hash(self.discord_id)
+
+
+def courses() -> set[Course]:
+    """Returns a list of all Courses held at JKU in the current semester."""
+    all_courses = set()
+
+    table = BeautifulSoup(requests.get("https://kusss.jku.at/kusss/coursecatalogue-search-lvas.action?").text,
+                          'html.parser').findAll("table")[5]
+
+    for row in table.find_all("tr")[1:]:
+        cols = row.findAll("td")
+        all_courses.add(Course(
+            lva_nr="".join(cols[0].text.strip().split(".")),
+            semester=cols[5].text.strip(),
+            lva_type=cols[2].text.strip(),
+            lva_name=" ".join(cols[1].text.split()),
+            teachers=list(map(lambda elem: elem.text.strip(), cols[4].findAll("a"))),
+            link=f'www.kusss.jku.at/kusss/{cols[0].find("a")["href"].strip()}'
+        ))
+
+    return all_courses
+
+
+def student(discord_id: str, link: str, matr_nr: str = None) -> Student:
+    """Returns a student object with keys to all courses which are taken in the current semester."""
+    url = urlparse(link)
+    if url.netloc != "www.kusss.jku.at" or url.path != "/kusss/published-calendar.action":
+        raise InvalidURLException(link, "Invalid URL.")
+
+    try:
+        user_token = parse_qs(url.query)["token"][0]
+    except Exception:
+        raise InvalidURLException(link, "Token not in query.")
+
+    link = f"https://www.kusss.jku.at/kusss/published-calendar.action?token={user_token}&lang=de"
+
+    i_calendar = requests.get(link).content
+    if len(i_calendar) == 0:
+        raise InvalidURLException(link, "Token malformed or deprecated.")
+
+    cal = Calendar.from_ical(i_calendar)
+    user_courses = set[CourseKey]()
+    cur_sem = current_semester()
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            summary = component.get('summary').split(" / ")
+
+            # delete tag of exams
+            if len(summary) > 3:
+                summary = summary[1:]
+
+            semester = summary[2][8:13]
+            if semester == cur_sem:
+                user_courses.add(CourseKey(
+                    lva_nr=summary[2][1:7],
+                    semester=semester
+                ))
+
+    return Student(
+        discord_id=discord_id,
+        calendar_link=link,
+        courses=list(user_courses),
+        matr_nr=matr_nr
+    )
+
+
+def current_semester() -> str:
+    return BeautifulSoup(requests.get("https://www.kusss.jku.at/kusss/coursecatalogue-start.action").text,
+                         'html.parser').find("option").text.strip()
