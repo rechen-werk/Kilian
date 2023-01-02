6cfbe6f1518a5354fba5966f53d48ea2ef5626da
Adrian Vinojcic <adrian.vinojcic@gmail.com>
2023-01-02 12:57:06 +0100

    classes can be obtained form course objects

--- kusss.py
+++ kusss.py
@@ -3,7 +3,7 @@
     Author: Adrian Vinojcic
     This part is responsible for fetching users calendars.
 """
-
+import datetime
 from urllib.parse import urlparse, parse_qs
 import requests
 from icalendar import Calendar
@@ -34,6 +34,28 @@ class CourseKey:
         return hash(self.lva_nr + self.semester)
 
 
+class Class(CourseKey):
+    def __init__(self, lva_nr: str, semester: str, start: datetime, end: datetime, location: str):
+        super().__init__(lva_nr, semester)
+        self.start = start
+        self.end = end
+        self.location = location
+
+    def __eq__(self, other):
+        return isinstance(other, Class) and \
+               self.lva_nr == other.lva_nr and \
+               self.semester == other.semester and \
+               self.start == other.start and \
+               self.end == other.end and \
+               self.location == other.location
+
+    def __ne__(self, other):
+        return not __eq__(other)
+
+    def __hash__(self):
+        return hash(self.lva_nr + self.semester + self.location) ^ hash(self.start) ^ hash(self.end)
+
+
 class Course(CourseKey):
     def __init__(self, lva_nr: str, semester: str, lva_type: str, lva_name: str, teachers: list[str], link: str):
         super().__init__(lva_nr, semester)
@@ -42,13 +64,33 @@ class Course(CourseKey):
         self.teachers = teachers
         self.link = link
 
+    def classes(self) -> set[Class]:
+        """Returns a set of all classes of the course."""
+        all_classes = set[Class]()
+        table = BeautifulSoup(requests.get(self.link).text, 'html.parser').findAll("table")[6]
+
+        for row in table.find_all("tr")[1:-1:2]:
+            cols = row.findAll("td")
+            date = cols[1].text.strip()
+            times = cols[2].text.strip().split(" – ")
+
+            all_classes.add(Class(
+                lva_nr=self.lva_nr,
+                semester=self.semester,
+                start=datetime.datetime.strptime(date + times[0], '%d.%m.%y%H:%M'),
+                end=datetime.datetime.strptime(date + times[1], '%d.%m.%y%H:%M'),
+                location=cols[3].text.strip()
+            ))
+
+        return all_classes
+
 
 class Student:
-    def __init__(self, discord_id: str, calendar_link: str, courses: list[CourseKey], matr_nr: str = None):
+    def __init__(self, discord_id: str, calendar_link: str, courses: set[CourseKey], student_id: str = None):
         self.discord_id = discord_id
         self.calendar_link = calendar_link
         self.courses = courses
-        self.matr_nr = matr_nr
+        self.student_id = student_id
 
     def __eq__(self, other):
         return isinstance(other, Student) and self.discord_id == other.discord_id
@@ -81,7 +123,7 @@ def courses() -> set[Course]:
     return all_courses
 
 
-def student(discord_id: str, link: str, matr_nr: str = None) -> Student:
+def student(discord_id: str, link: str, student_id: str = None) -> Student:
     """Returns a student object with keys to all courses which are taken in the current semester."""
     url = urlparse(link)
     if url.netloc != "www.kusss.jku.at" or url.path != "/kusss/published-calendar.action":
@@ -119,8 +161,8 @@ def student(discord_id: str, link: str, matr_nr: str = None) -> Student:
     return Student(
         discord_id=discord_id,
         calendar_link=link,
-        courses=list(user_courses),
-        matr_nr=matr_nr
+        courses=user_courses,
+        student_id=student_id
     )
 
 
