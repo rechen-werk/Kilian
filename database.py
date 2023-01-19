334e845753690ba9b330ba4b391db2b8feac0159
Tobias Pilz <k11914471@students.jku.at>
2023-01-19 02:12:15 +0100

    Refactoring, fixing, headache

--- database.py
+++ database.py
@@ -7,90 +7,38 @@ import sqlite3
 
 import kusss
 from kusss import Student, Course, Class
+import sql_queries as query
 
 __DB__ = "discord.db"
-
-__insert_student_query__ = "REPLACE INTO " \
-                           "student(discord_id, student_id, calendar_link) " \
-                           "VALUES (?,?,?)"
-
-__insert_course_query__ = "REPLACE INTO " \
-                          "course(lva_nr, semester, lva_type, lva_name, link) " \
-                          "VALUES (?,?,?,?,?)"
-
-__insert_student_courses_query__ = "REPLACE INTO " \
-                          "student_courses(discord_id, lva_nr, semester) " \
-                          "VALUES (?,?,?)"
-
-__insert_class_query__ = "REPLACE INTO " \
-                         "class(lva_nr, semester, start_time, end_time, location) " \
-                         "VALUES (?,?,?,?)"
 
 
 class Database:
     def __init__(self):
         self.__con__ = sqlite3.connect(__DB__)
         self.__cur__ = self.__con__.cursor()
+        self.__cur__.execute("PRAGMA foreign_keys = ON")
         self.__create_tables__()
         print("Pulling data from KUSSS")
-        self.__cur__.executemany(__insert_course_query__, [c.to_db_entry() for c in kusss.courses()])
+        self.__cur__.executemany(query.insert_course, [c.to_db_entry() for c in kusss.courses()])
+        self.__con__.commit()
         print("Update complete")
 
     def __create_tables__(self):
-        self.__cur__.execute(
-            "CREATE TABLE IF NOT EXISTS student("
-            "discord_id text not null, "  # integer?
-            "student_id text nullable, "
-            "calendar_link text not null,"
-            "primary key (discord_id)"
-            ")"
-        )
-        self.__cur__.execute(
-            "CREATE TABLE IF NOT EXISTS course("
-            "lva_nr text not null, "
-            "semester text not null, "
-            "lva_type text not null , "
-            "lva_name text not null , "
-            "link text not null, "
-            "primary key (lva_nr, semester)"
-            ")"
-        )
-        self.__cur__.execute(
-            "CREATE TABLE IF NOT EXISTS student_courses("
-            "discord_id text not null, "
-            "semester text not null, "
-            "lva_nr text not null, "
-            "foreign key (discord_id) references student(discord_id),"
-            "foreign key (semester, lva_nr) references course(semester, lva_nr)"
-            ")"
-        )
-        self.__cur__.execute(
-            "CREATE TABLE IF NOT EXISTS course_teachers("
-            "teacher_name text unique not null, "  # maybe not unique?
-            "semester text not null,"
-            "lva_nr integer not null, "
-            "primary key (teacher_name, semester, lva_nr)"
-            ")"
-        )
-        self.__cur__.execute(
-            "CREATE TABLE IF NOT EXISTS class("
-            "lva_nr text not null, "
-            "semester text not null, "
-            "start_time integer not null, "
-            "end_time integer not null, "
-            "location text not null,"
-            "primary key (lva_nr, semester, start_time, end_time, location)"
-            ")"
-        )
+        self.__cur__.execute(query.create_student)
+        self.__cur__.execute(query.create_course)
+        self.__cur__.execute(query.create_student_courses)
+        self.__cur__.execute(query.create_course_teacher)
+        self.__cur__.execute(query.create_class)
+        self.__cur__.execute(query.create_roles)
 
     def insert(self, obj):
         if isinstance(obj, Student):
-            self.__cur__.execute(__insert_student_query__, obj.to_db_entry())
-            self.__cur__.executemany(__insert_student_courses_query__, [(obj.discord_id, course.lva_nr, course.semester) for course in obj.courses])
+            self.__cur__.execute(query.insert_student, obj.to_db_entry())
+            self.__cur__.executemany(query.insert_student_courses, [(obj.discord_id, course.lva_nr, course.semester) for course in obj.courses])
         elif isinstance(obj, Course):
-            self.__cur__.execute(__insert_course_query__, obj.to_db_entry())
+            self.__cur__.execute(query.insert_course, obj.to_db_entry())
         elif isinstance(obj, Class):
-            self.__cur__.execute(__insert_class_query__, obj.to_db_entry())
+            self.__cur__.execute(query.insert_class, obj.to_db_entry())
         else:
             return NotImplemented
         self.__con__.commit()
@@ -101,12 +49,16 @@ class Database:
 
 
 if __name__ == "__main__":
+    db = None
     try:
-        c1 = Course("234.345", "WS22", "KV", "Course1", [], "c1.live.com")
-        c2 = Course("234.346", "WS22", "KV", "Course2", [], "c2.evil.com")
-        student = Student("lel", "evil.org", {c1, c2}, "k12345678")
         db = Database()
-        db.insert(student)
-        db.close()
+        c1 = Course("234345", "WS22", "KV", "Course1", [], "c1.live.com")
+        c2 = Course("234346", "WS22", "KV", "Course2", [], "c2.evil.com")
+        student = Student("lel", "evil.org", {c1, c2}, "k12345678")
+        db.insert(student)  # fails because of foreign key constraint
     except sqlite3.OperationalError as err:
         print(f"[{err.sqlite_errorname}]\t{err.args[0]}")
+    finally:
+        if db:
+            db.close()
+        print("finished execution")
