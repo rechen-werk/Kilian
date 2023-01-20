73a3f4eee92a6e94d64e043151cd8bece21f0a46
Tobias Pilz <k11914471@students.jku.at>
2023-01-20 16:49:36 +0100

    cleanup

--- kilian.py
+++ kilian.py
@@ -12,13 +12,7 @@ from database import Database
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-t", "--token",
-        type=str,
-        required=False,
-        dest='token',
-        help="Provide the Discord Bot Token as string."
-    )
+    parser.add_argument("-t", "--token", type=str, required=False, dest='token', help="Provide the Discord Bot Token as string.")
     return parser.parse_args()
 
 
@@ -35,6 +29,7 @@ if __name__ == '__main__':
 
     bot = interactions.Client(token=bot_token, intents=interactions.Intents.ALL)
 
+
     @bot.command()
     @interactions.option(description="Provide the calendar link from KUSSS here.")
     @interactions.option(description="Optionally provide your matriculation number here.")
@@ -45,17 +40,16 @@ if __name__ == '__main__':
             database.insert(student)
             for course_key in student.courses:
                 for guild in bot.guilds:
-                    course: uni.Course = None # get course from key in course_key
+                    course: uni.Course = None  # get course from key in course_key
 
                     # if role does not exist on this server
                     # role = await guild.create_role(name=course.lva_name, color=0xD8E4FF)
-                    guild_id = guild.id
-                    # role_id = role.id
-                    # database.insert()
+                    guild_id = guild.id  # role_id = role.id  # database.insert()
 
             await ctx.send("Welcome on board " + ctx.author.name + "!")
         except uni.InvalidURLException as ex:
             await ctx.send(ex.message, ephemeral=True)
+
 
     @bot.command()
     async def unkusss(ctx: interactions.CommandContext):
@@ -67,6 +61,7 @@ if __name__ == '__main__':
 
         await ctx.send("A pity to see you leave " + ctx.author.name + ". You can join the club anytime with `/kusss`!")
 
+
     @bot.command()
     @interactions.option(description="Role you want to ping.")
     @interactions.option(description="Message content goes here.")
@@ -75,25 +70,21 @@ if __name__ == '__main__':
         role_id = str(role.id)
         guild_id = str(ctx.guild_id)
 
-
-        # DONE: check if said role is a managed role and send an ephemeral errormessage otherwise
-        # DONE: get all users mapped to said *role_id*
-        users_with_anonymous_role = set()
         if database.is_managed_role(guild_id, role_id):
             users_with_anonymous_role = database.get_role_members(guild_id, role_id)
+        else:
+            # TODO: send an ephemeral errormessage
+            return NotImplemented
 
         ping_string = ""
         for user_id in users_with_anonymous_role:
             user = (await ctx.guild.get_member(int(user_id))).user
             ping_string += user.mention
 
-        # DONE: ping all users with said role in one message
+        await ctx.send(ping_string + "\n" + content)
 
-        message = await ctx.send(ping_string + "\n" + content)
-        # await message.edit(role.mention + "\n" + content)
+        # POSSIBLE ERROR: too many users, so that not all pings fit in one message  # POSSIBLE SOLUTION TO ERROR: give all pinged users the role temporarily, ping them, and remove the role
 
-        # POSSIBLE ERROR: too many users, so that not all pings fit in one message
-        # POSSIBLE SOLUTION TO ERROR: give all pinged users the role temporarily, ping them, and remove the role
 
     @bot.command()
     @interactions.option(description="The user you want the student id of.")
@@ -123,6 +114,12 @@ if __name__ == '__main__':
 
     @bot.event()
     async def on_message_create(message: interactions.Message):
+        """
+        Checks if a message contains a managed mention
+        If yes: pings all users by replying to the message sent
+        If no: returns
+        """
+
         if message.author.id == bot.me.id:
             return
 
@@ -132,33 +129,22 @@ if __name__ == '__main__':
         if len(mentioned_roles) == 0:
             return
 
-        # DONE: check if a role is a managed role
-        # DONE: get all users mapped to said *role_id*
-
         users_with_anonymous_role = set()
         for role_id in mentioned_roles:
             users_with_anonymous_role = users_with_anonymous_role | database.get_role_members(guild_id, role_id)
-
-        # DONE: ping all users with said role in one message
-        await message.reply("ok ok")
 
         ping_string = ""
         for user_id in users_with_anonymous_role:
             user = (await guild.get_member(int(user_id))).user
             ping_string += user.mention
 
-        channel = await message.get_channel()
+        await message.reply(ping_string)
 
-        content = message.content
-        # await message.delete()
-        message = await channel.send(ping_string)
-        await message.edit(content)
 
     @bot.event()
     async def on_guild_create(guild: interactions.Guild):
         for member in guild.members:
             user_id = member.id
-
 
 
     @bot.event()
